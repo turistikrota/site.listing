f237e4c6f874220d592b1e7058ccd229f5800593
9ssi7 <info@ssibrahimbas.com>
2023-12-15 20:27:30 +0300

    feature/init: add config folder

---
+++ src/config/services.ts
@@ -0,0 +1,16 @@
+export enum Services {
+    Auth = 'auth',
+    Account = 'account',
+    Place = 'place',
+    Listing = 'listing'
+  }
+  
+  export const ApiUrls: Record<Services, string> = {
+    [Services.Auth]: process.env.NEXT_PUBLIC_AUTH_API_URL!,
+    [Services.Account]: process.env.NEXT_PUBLIC_ACCOUNT_API_URL!,
+    [Services.Place]: process.env.NEXT_PUBLIC_PLACE_API_URL!,
+    [Services.Listing]: process.env.NEXT_PUBLIC_LISTING_API_URL!,
+  }
+  
+  export const apiUrl = (service: Services, path: string) => `${ApiUrls[service]}${path}`
+  
