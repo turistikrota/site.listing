4709a6c168c244dd823444bee71a9def4081d307
9ssi7 <info@ssibrahimbas.com>
2023-12-15 20:28:54 +0300

    feature/init: add styles dir feature/init: add types dir

---
+++ src/types/error.ts
@@ -0,0 +1,22 @@
+type ApiError = {
+    response: {
+      data: any
+    }
+  }
+  
+  type ValidationErrorEl = {
+    field: string
+    message: string
+    namespace: string
+  }
+  
+  type ValidationError = ValidationErrorEl[]
+  
+  export function isApiError(error: any): error is ApiError {
+    return error.response && error.response.data
+  }
+  
+  export function isValidationError(error: any): error is ValidationErrorEl[] {
+    return Array.isArray(error) && error.length > 0 && error[0].field && error[0].message
+  }
+  
