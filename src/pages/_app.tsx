ed28f21c73780237da389f7fe5af240270c9cf72
9ssi7 <info@ssibrahimbas.com>
2023-12-16 12:18:07 +0300

    feature/init: complete i18n analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import '@turistikrota/fonts/verdana.css'
 import '@turistikrota/ui/assets/config.css'
 import '@turistikrota/ui/assets/default.css'
+import 'boxicons/css/boxicons.min.css'
+import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
 import '~/styles/globals.css'
+import '~/styles/leaflet.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+export default appWithTranslation(App)
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,61 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import CubeEffect from '@turistikrota/ui/design/cube'
+import GlassEffect from '@turistikrota/ui/design/glass'
+import { useTranslation } from 'next-i18next'
+import { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+import type { FC } from 'react'
+
+const TurkishAnalytics: FC = () => {
+  return (
+    <>
+      <Script
+        async={true}
+        src='https://www.googletagmanager.com/gtag/js?id=G-2NMFRCYRF2'
+        strategy='afterInteractive'
+      ></Script>
+      <Script id='google-tag' strategy='afterInteractive'>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-2NMFRCYRF2');
+      `}
+      </Script>
+    </>
+  )
+}
+
+const EnglishAnalytics: FC = () => {
+  return (
+    <>
+      <Script
+        async={true}
+        src='https://www.googletagmanager.com/gtag/js?id=G-Q7YKQ763VG'
+        strategy='afterInteractive'
+      ></Script>
+      <Script id='google-tag' strategy='afterInteractive'>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-Q7YKQ763VG');
+        `}
+      </Script>
+    </>
+  )
+}
 
 export default function Document() {
+  const { i18n } = useTranslation()
   return (
-    <Html lang="en">
+    <Html lang={i18n.language}>
       <Head />
       <body>
+        <GlassEffect.Fixed />
+        <CubeEffect.All />
         <Main />
         <NextScript />
+        {i18n.language === 'tr' ? <TurkishAnalytics /> : <EnglishAnalytics />}
       </body>
     </Html>
   )
