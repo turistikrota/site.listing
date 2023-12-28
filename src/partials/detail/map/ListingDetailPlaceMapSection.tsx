0362a1c3a13efe1f85404649495f8d341e3b8bb7
9ssi7 <info@ssibrahimbas.com>
2023-12-29 02:59:28 +0300

    feature/detail-view: update map location pins

--- src/partials/detail/map/ListingDetailPlaceMapSection.tsx
+++ src/partials/detail/map/ListingDetailPlaceMapSection.tsx
@@ -1,6 +1,7 @@
 import { Coordinates } from '@turistikrota/location-tr'
-import { LatLngTuple } from 'leaflet'
+import { Icon, LatLngTuple } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+import { useTranslation } from 'next-i18next'
 import dynamic from 'next/dynamic'
 import { FC } from 'react'
 import { Circle, Marker, Popup } from 'react-leaflet'
@@ -20,12 +21,24 @@ type Props = {
 }
 
 const ListingDetailPlaceMapSection: FC<Props> = ({ data, position, listingCoordinates, isStrict }) => {
+  const { t } = useTranslation('listing')
   return (
     <div className='h-144 md:h-164'>
       <DynamicMap position={position} zoom={15}>
         <MapDefaultConfig />
 
-        {isStrict && <Marker position={listingCoordinates} />}
+        {isStrict && <Marker position={listingCoordinates} icon={new Icon.Default({
+          iconUrl: '/images/marker/red.png',
+          iconSize: [48, 48],
+          iconAnchor: [24, 48],
+          iconRetinaUrl: '/images/marker/red.png',
+        })}>
+          <Popup className='map-popup bg-second p-2 rounded-md flex items-center text-center justify-center' closeButton={false}>
+            <div className='w-full text-center'>
+              {t('sections.map.strict')}
+            </div>
+          </Popup>
+          </Marker>}
         {!isStrict && <Circle center={listingCoordinates} radius={200} color='#2e99fd' />}
 
         {data &&
